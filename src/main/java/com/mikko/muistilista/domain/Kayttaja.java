0823e9bd371e2fd7e2c4950c6edbc6ad0ac38e3c
massakeisari <mikkotakala@hotmail.com>
2017-03-27 11:01:43 +0300

    luokkia

---
+++ src/main/java/com/mikko/muistilista/domain/Kayttaja.java
@@ -0,0 +1,24 @@
+package com.mikko.muistilista.domain;
+
+public class Kayttaja {
+    private int id;
+    private String nimi;
+    private String salasana;
+    
+    public Kayttaja(int id, String nimi, String salasana) {
+        this.nimi = nimi;
+        this.salasana = salasana;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public String getSalasana() {
+        return this.salasana;
+    }
+}
---
+++ src/main/java/com/mikko/muistilista/domain/Muistettava.java
@@ -0,0 +1,38 @@
+package com.mikko.muistilista.domain;
+
+public class Muistettava {
+    private int id;
+    private int kayttajaId;
+    private String nimi;
+    private boolean tehty;
+    private String kuvaus;
+    
+    public Muistettava(int id, int kayttajaId, String nimi, boolean tehty,
+            String kuvaus) {
+        this.id = id;
+        this.kayttajaId = kayttajaId;
+        this.nimi = nimi;
+        this.tehty = false;
+        this.kuvaus = kuvaus;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+    
+    public int getkayttajaId() {
+        return this.kayttajaId;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public boolean getTehty() {
+        return this.tehty;
+    }
+    
+    public String getKuvaus() {
+        return this.kuvaus;
+    }
+}
