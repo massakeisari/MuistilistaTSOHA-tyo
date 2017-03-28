013873fd5673fdc1a7187633827beaea752962fd
massakeisari <mikkotakala@hotmail.com>
2017-03-28 16:25:52 +0300

    doc

--- src/main/java/com/mikko/muistilista/database/KayttajaDao.java
+++ src/main/java/com/mikko/muistilista/database/KayttajaDao.java
@@ -1,10 +1,83 @@
 package com.mikko.muistilista.database;
 
+import com.mikko.muistilista.domain.Kayttaja;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class KayttajaDao {
+
+public class KayttajaDao implements Dao<Kayttaja, Integer>{
     private Database db;
     
     public KayttajaDao(Database db) {
         this.db = db;
+    }
+    
+    public void lisaaKayttaja(String nimi, String salasana) throws SQLException {
+        Connection conn = db.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO Kayttaja(nimi, salasana)"
+                + "VALUES(?, ?)");
+        stmt.setString(1, nimi);
+        stmt.setString(2, salasana);
+        
+        stmt.execute();
+        
+        stmt.close();
+        conn.close();
+    }
+    
+    @Override
+    public void delete(Integer key) throws SQLException {
+        Connection conn = db.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM Kayttaja WHERE id = ?");
+        stmt.setInt(1, key);
+        stmt.execute();
+        stmt.close();
+        conn.close();
+    }
+    
+    @Override
+    public List<Kayttaja> findAll() throws SQLException {
+        Connection conn = db.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Kayttaja");
+        ResultSet rs = stmt.executeQuery();
+        List<Kayttaja> kayttajat  = new ArrayList<>();
+        
+        while(rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+            String salasana = rs.getString("salasana");
+
+            kayttajat.add(new Kayttaja(id, nimi, salasana));
+        }
+        stmt.close();
+        conn.close();
+        
+        return kayttajat;
+    }
+    
+    @Override
+    public Kayttaja findOne(Integer key) throws SQLException {
+        Connection conn = db.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Kayttaja WHERE id = ?");
+        stmt.setObject(1, key);
+        ResultSet rs = stmt.executeQuery();
+        if(!rs.next()) {
+            return null;
+        }
+        
+        int id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+        String salasana = rs.getString("salasana");
+        
+        Kayttaja k = new Kayttaja(id, nimi, salasana);
+        
+        rs.close();
+        stmt.close();
+        conn.close();
+        return k;
     }
 }
