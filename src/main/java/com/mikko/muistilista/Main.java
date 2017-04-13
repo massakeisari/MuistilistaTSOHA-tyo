c169620b8d103897afb07ac1b5f6eea3ab261e93
Mikko Takala <mikko.takala@helsinki.fi>
2017-04-13 12:54:57 +0300

    gggg

--- src/main/java/com/mikko/muistilista/Main.java
+++ src/main/java/com/mikko/muistilista/Main.java
@@ -12,154 +12,149 @@ import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-
 public class Main {
-    
-    
+
     public static void main(String[] args) throws Exception {
         if (System.getenv("PORT") != null) {
             port(Integer.valueOf(System.getenv("PORT")));
         }
-        
+
         String jdbcOsoite = "";
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
         if (System.getenv("DATABASE_URL") != null) {
             //jdbcOsoite = System.getenv("DATABASE_URL");
             String username = dbUri.getUserInfo().split(":")[0];
             String password = dbUri.getUserInfo().split(":")[1];
-            jdbcOsoite = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +
-                    "?user=" + username + "&password=" + password;
-        } 
+            jdbcOsoite = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+                    + "?user=" + username + "&password=" + password;
+        }
         Database database = new Database(jdbcOsoite);
         database.setDebugMode(true);
         KayttajaDao kd = new KayttajaDao(database);
         MuistettavaDao md = new MuistettavaDao(database);
         Tarkastaja tark = new Tarkastaja();
-        
+
         //Etusivu
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
-            
+
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
+
         //Listasivu
         //TODO - jos ei kirjautunut, ohjaa kirjautumissivulle
         get("/lista", (req, res) -> {
             HashMap map = new HashMap<>();
-            
-            Kayttaja k = (Kayttaja)req.session().attribute("kirj");
-            
-            if(k == null) {
+
+            Kayttaja k = (Kayttaja) req.session().attribute("kirj");
+
+            if (k == null) {
                 res.redirect("/kirjautuminen");
             } else {
                 res.redirect("/kayttaja/" + k.getId());
             }
-            
+
             return new ModelAndView(map, "lista");
         }, new ThymeleafTemplateEngine());
-        
+
         //Etusivu
         get("/index", (req, res) -> {
             HashMap map = new HashMap<>();
-            
+
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
-        
+
         //Kirjautumissivu
         get("/kirjautuminen", (req, res) -> {
             HashMap map = new HashMap<>();
-            
+
             return new ModelAndView(map, "kirjautuminen");
         }, new ThymeleafTemplateEngine());
-        
+
         //Kirjautuminen ja listaan redirectaaminen
         post("/login", (req, res) -> {
             String nimi = req.queryParams("nimi");
             String salasana = req.queryParams("salasana");
-            
+
             Kayttaja k = kd.findByNamePass(nimi, salasana);
-            
-            if(k == null) {
+
+            if (k == null) {
                 res.redirect("/kirjautuminen");
                 return "";
             }
-            
+
             req.session(true).attribute("kirj", k);
             res.redirect("/kayttaja/" + k.getId());
             return "";
         });
-        
+
         //Kirjautuneen käyttäjän oma lista
         get("/kayttaja/:id", (req, res) -> {
             HashMap map = new HashMap<>();
-            Kayttaja kirj = (Kayttaja)req.session().attribute("kirj");
+            Kayttaja kirj = (Kayttaja) req.session().attribute("kirj");
             int kayttajaid = kirj.getId();
-            //int kayttajaid = Integer.parseInt(req.params(":id"))+1;
             Kayttaja k = kd.findOne(kayttajaid);
             System.out.println(k.toString());
+
             map.put("kayttaja", k.getNimi());
             map.put("muistettavat", md.findByKayttajaId(kayttajaid));
             map.put("lisaa", "Lisaa muistettava");
-            
+
             return new ModelAndView(map, "lista");
         }, new ThymeleafTemplateEngine());
-        
+
         //Rekisteröintisivu
         get("/rekisterointi", (req, res) -> {
             HashMap map = new HashMap<>();
             return new ModelAndView(map, "rekisterointi");
         }, new ThymeleafTemplateEngine());
-        
+
         //Rekisteröinti lomakkeella
         //TODO - Vääränlaisen nimen syöttämisen redirectauksen jälkeen
         //ilmoitus asiasta
         post("/rek", (req, res) -> {
             String nimi = req.queryParams("nimi");
-            if(!tark.tarkastaNimi(nimi)) {
+            if (!tark.tarkastaNimi(nimi)) {
                 res.redirect("/rekisterointi");
             }
             String salasana = req.queryParams("salasana");
             kd.lisaaKayttaja(nimi, salasana);
-            
+
             res.redirect("/kirjautuminen");
             return "";
         });
-        
-        
-        
+
         //TODO - Muistettavan lisäys tietokantaan lomakkeella
         //TODO - Vääränlaisen nimen syöttämisen redirectauksen jälkeen
         //ilmoitus asiasta
         post("/lisaam", (req, res) -> {
             String nimi = req.queryParams("nimi");
-            Kayttaja kirj = (Kayttaja)req.session().attribute("kirj");
-            if(!tark.tarkastaMuistettava(nimi)) {
+            Kayttaja kirj = (Kayttaja) req.session().attribute("kirj");
+            if (!tark.tarkastaMuistettava(nimi)) {
                 res.redirect("/kayttaja/" + kirj.getId());
             }
             String kuvaus = req.queryParams("kuvaus");
-            
+
             md.lisaa(kirj.getId(), nimi, kuvaus);
-            
+
             res.redirect("/kayttaja/" + kirj.getId());
             return "";
         });
-        
+
         //Muistettavan "Poista" -napille ei valmis
         post("/poista", (req, res) -> {
             int mId = Integer.parseInt(req.queryParams("id"));
             md.delete(mId);
-            
-            Kayttaja k = (Kayttaja)req.session().attribute("kirj");
+
+            Kayttaja k = (Kayttaja) req.session().attribute("kirj");
             res.redirect("/kayttaja/" + k.getId());
             return "";
         });
-        
+
         get("/lisaamuistettava", (req, res) -> {
-           HashMap map = new HashMap<>();
-           
-           return new ModelAndView(map, "lisaamuistettava");
+            HashMap map = new HashMap<>();
+
+            return new ModelAndView(map, "lisaamuistettava");
         }, new ThymeleafTemplateEngine());
     }
 }
